7e2fd0a7feddb81968a29a4d411c711bc2b53bfd
srijani07 <srijaniparui07@gmail.com>
2021-07-03 00:02:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var btnTranslate = document.querySelector('#translate-button');
-var txtInput = document.querySelector('#txt-input');
-var outputDiv = document.querySelector('#output');
+var btnTranslate = document.querySelector('#translate-button'); //capturing button interaction
+var txtInput = document.querySelector('#txt-input'); 
+var outputDiv = document.querySelector('#output'); //for displaying output
 var serverURL= "https://api.funtranslations.com/translate/yoda.json";
 function getTranslationURL(text) {
 return serverURL + "?" +"text=" + text;
@@ -12,11 +12,12 @@ function errorHandler(error) {
 }
 
 function clickHandler() {
-    var inputText = txtInput.value;
-    fetch(getTranslationURL(inputText))
+
+    var inputText = txtInput.value; //taking the user input value
+    fetch(getTranslationURL(inputText)) //calling the API 
      .then(response => response.json())
      .then(json => {
-        var translatedText = json.contents.translated;
+        var translatedText = json.contents.translated; //storing the translated sentence
         outputDiv.innerText = translatedText; 
 })
 
